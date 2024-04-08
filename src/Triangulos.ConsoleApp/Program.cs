0e55234f7db961dc2389637d8bfd99a0e6f3c4cc
Leandro Teixeira Wolff <leandro.teixeira.wolff@hotmail.com>
2024-04-08 17:57:40 -0300

    Leandro Wolff

--- src/Triangulos.ConsoleApp/Program.cs
+++ src/Triangulos.ConsoleApp/Program.cs
@@ -4,7 +4,20 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Digite os valores dos lados de um triângulo, para descobrir qual tipo ele é: ");
+            double x = Convert.ToDouble(Console.ReadLine());
+            double y = Convert.ToDouble(Console.ReadLine());
+            double z = Convert.ToDouble(Console.ReadLine());
+
+            Triangulo criarTriangulo = new Triangulo();
+
+            criarTriangulo.x = x;
+            criarTriangulo.y = y;
+            criarTriangulo.z = z;
+
+            string tipoDeTriangulo = criarTriangulo.TipoDeTriangulo();
+
+            Console.WriteLine(tipoDeTriangulo);
         }
     }
 }
---
+++ src/Triangulos.ConsoleApp/Triangulo.cs
@@ -0,0 +1,19 @@
+﻿namespace Triangulos.ConsoleApp
+{
+    class Triangulo
+    {
+        public double x, y, z;
+
+        public string TipoDeTriangulo()
+        {
+            if (x > y + z || y > x + z || z > x + y)
+                return "Triângulo Inválido!";
+            else if (x != y && x != z && y != z)
+                return "Triângulo Escaleno!";
+            else if (x == y && y == z)
+                return "Triângulo Equilátero!";
+            else
+                return "Triângulo Isósceles";
+        }
+    }
+}
